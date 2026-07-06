df62774beba7a85a4f228040dd48f9effb9198b7
GreenL <64342473+GreenL-MSD@users.noreply.github.com>
2026-07-06 17:27:41 +0100

    Added ore definition JSON files

--- src/main/java/io/github/loganmartingreen/geovein/data/OreDefinition.java
+++ src/main/java/io/github/loganmartingreen/geovein/data/OreDefinition.java
@@ -1,8 +1,16 @@
 package io.github.loganmartingreen.geovein.data;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
 public record OreDefinition(
         String id,
         String displayName,
         String sourceOreBlock
 ) {
+    public static final Codec<OreDefinition> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Codec.STRING.fieldOf("id").forGetter(OreDefinition::id),
+            Codec.STRING.fieldOf("display_name").forGetter(OreDefinition::displayName),
+            Codec.STRING.fieldOf("source_ore_block").forGetter(OreDefinition::sourceOreBlock)
+    ).apply(instance, OreDefinition::new));
 }
